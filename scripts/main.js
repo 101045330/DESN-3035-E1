d540ca774f14412cf88acfe9556cfba76548dc80
101045330 <101045330@georgebrown.ca>
2024-09-10 23:27:55 +0000

    script commented

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/*alert message on main branch navigation until allowed */
 document.getElementById('clickMe').addEventListener('click', function() {
     alert('Sorry, No access to the root! -MIN');
 }); 
